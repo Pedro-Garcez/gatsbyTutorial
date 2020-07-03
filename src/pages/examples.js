70062f6edfba590d96e271564056db42619c799d
Pedro <pedropaulogarcez@gmail.com>
2020-07-03 17:32:55 -0400

    Added layout component to examples page.

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Header from "../examples/Header"
+import Layout from "../components/Layout"
 
 const examples = () => {
   return (
-    <div>
-      <h1>hello from examples page</h1>
-      <Header />
-    </div>
+    <Layout>
+      <div>
+        <h1>hello from examples page</h1>
+        <Header />
+      </div>
+    </Layout>
   )
 }
 
