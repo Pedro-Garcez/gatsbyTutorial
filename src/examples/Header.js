dfae444ad202b4bdd14546af047bb3f2fd75c88a
Pedro <pedropaulogarcez@gmail.com>
2020-07-04 14:21:31 -0400

    created a function that gets our data from graphql and assigned it to getData. assigned useStaticQuery to a variable and passed it our getData function

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,7 +1,25 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 
 const Header = () => {
+  const data = useStaticQuery(getData)
+  console.log(data)
   return (
     <div>
       <h1>this is a heading</h1>
