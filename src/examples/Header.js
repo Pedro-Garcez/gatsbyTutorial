a9f02630029e9e12b965b04750b00f41e3c4aeda
Pedro <pedropaulogarcez@gmail.com>
2020-07-04 14:00:49 -0400

    setting up graphql query in header.js from scratch to better understand how it works.

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -2,26 +2,9 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          data
-          description
-          title
-          person {
-            age
-            name
-          }
-        }
-      }
-    }
-  `)
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
-      <h2>{data.site.siteMetadata.person.age}</h2>
+      <h1>this is a heading</h1>
     </div>
   )
 }
