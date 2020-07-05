2ef235be703920e6126d942633441681ca092276
Pedro <pedropaulogarcez@gmail.com>
2020-07-05 17:45:15 -0400

    used graphql page query and exported the data. Used props to access it on the examples page.

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,35 @@ import React from "react"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = (props) => {
+  console.log(props)
   return (
     <Layout>
       <h1>hello from examples page</h1>
       <Header />
       <HeaderStatic />
+      <pre>{JSON.stringify(props.data, null, 4)}</pre>
+      <h5>{props.data.site.info.author}</h5>
     </Layout>
   )
 }
 
+export const data = graphql`
+  query MyQuery {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 export default examples
